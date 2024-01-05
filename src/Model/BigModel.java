8f201a0a74762ba13632c217dbc41c8cfefa1454
MIHNEA-CPC\mihne <mihneabostina5@gmail.com>
2024-01-05 20:10:03 +0200

    misto rau

--- src/Model/BigModel.java
+++ src/Model/BigModel.java
@@ -18,7 +18,7 @@ public class BigModel {
 
     public BigModel() {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/proiect_v1", "root", "hd15mky.Mihibosti28");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/proiect_v1", "root", "?");
         } catch(SQLException se) {
             se.printStackTrace();
         }
