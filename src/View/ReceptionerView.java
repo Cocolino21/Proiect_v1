a880961cdbf8f6ffe0c9e67fa94e8a78156708fd
DESKTOP-L07CVPT\Tibi <tiberiubj@gmail.com>
2024-01-10 18:41:06 +0200

    ReceptionerView e gata :)

--- src/View/ReceptionerView.java
+++ src/View/ReceptionerView.java
@@ -2,17 +2,54 @@ package View;
 
 import Model.CurrentAngajat;
 import net.miginfocom.swing.MigLayout;
+import org.jdesktop.swingx.JXDatePicker;
+import org.jdesktop.swingx.JXTable;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-public class ReceptionerView extends BasicView{
+public class ReceptionerView extends BasicView {
     private JPanel receptionerM3Panel;
     private JFrame adaugaPacientFrame;
+
+
+    //////////////////////////////////////////////////////////
     private JFrame adaugaProgramareFrame;
+    private JPanel adaugaProgramarePanel;
+    private JComboBox<String> idMedicCB;
+    private JComboBox<String> idPacientCB;
+    private JXDatePicker dataDP;
+    private JTextField oraTF;
+    private JLabel idReceptioner;
+    private JLabel idPacient;
+    private JLabel idMedic;
+    private JLabel ora;
+    private JLabel data;
+    private JButton adaugaProgramare;
+    private JButton stergeProgramare;
+
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+    private JPanel adaugaPacientPanel;
+    private JButton adaugaPacient;
+    private JLabel numeLabel;
+    private JLabel prenumeLabel;
+    private JLabel nrTelefonLabel;
+    private JLabel cnpLabel;
+    private JLabel emailLabel;
+    private JTextField numeTF;
+    private JTextField prenumeTF;
+    private JTextField nrTelefonTF;
+    private JTextField cnpTF;
+    private JTextField emailTF;
+///////////////////////////////////////////////////////////////
     private JButton buttonAdaugaPacient= new JButton("Adauga pacient");
     private JButton buttonAdaugaProgramare= new JButton("Adauga programare");
+    private JButton buttonStergeProgramare=new JButton("Sterge programare");
+    private JComboBox<String> selectNumeCB = new JComboBox<>();
     private DefaultTableModel programariTableModel;
     private JScrollPane programariSP;
     private JTable programariTable;
@@ -36,14 +73,83 @@ public class ReceptionerView extends BasicView{
         updateProgramariTable();
 
         receptionerM3Panel.add(buttonAdaugaPacient, "span");
-        receptionerM3Panel.add(buttonAdaugaProgramare, "span");
-        receptionerM3Panel.add(programariSP);
+        receptionerM3Panel.add(buttonAdaugaProgramare);
+        receptionerM3Panel.add(selectNumeCB, "span");
+        receptionerM3Panel.add(programariSP, "span");
+        receptionerM3Panel.add(buttonStergeProgramare,"span");
 
 
-        this.adaugaPacientFrame = new JFrame("Adauga pacient");
+        adaugaPacientFrame = new JFrame("Adauga pacient");
+        adaugaPacientPanel = new JPanel(new MigLayout());
+        adaugaProgramareFrame = new JFrame ("Adauga programare");
+        adaugaProgramarePanel = new JPanel(new MigLayout());
 
-        this.adaugaProgramareFrame = new JFrame("Adauga programare");
-        adaugaProgramareFrame.add(new JLabel("adaugi programari aici"));
+        /////  INCEPUT FEREASTRA CU ADAUGARE PROGRAMARE
+        idReceptioner = new JLabel("id Receptioner");
+        idPacient = new JLabel("id Pacient");
+        idMedic = new JLabel("id Medic");
+        ora = new JLabel("Ora");
+        data = new JLabel("Data");
+        adaugaProgramare = new JButton("Adauga");
+        stergeProgramare = new JButton("Sterge");
+
+        idPacientCB = new JComboBox();
+        idMedicCB = new JComboBox();
+        oraTF = new JTextField();
+        dataDP = new JXDatePicker();
+
+
+        adaugaProgramarePanel.add(idMedic);
+        adaugaProgramarePanel.add(idMedicCB, "span");
+        adaugaProgramarePanel.add(idPacient);
+        adaugaProgramarePanel.add(idPacientCB, "span");
+        adaugaProgramarePanel.add(data);
+        adaugaProgramarePanel.add(dataDP, "span");
+        adaugaProgramarePanel.add(ora);
+        adaugaProgramarePanel.add(oraTF, "span");
+        adaugaProgramarePanel.add(adaugaProgramare);
+
+        adaugaProgramareFrame.setContentPane(adaugaProgramarePanel);
+        adaugaProgramareFrame.pack();
+        adaugaProgramareFrame.setLocationRelativeTo(null);
+        adaugaProgramareFrame.setResizable(false);
+        //// SFARSIT FEREASTRA ADAUGARE PROGRAMARE
+
+        //// INCEPUT FEREASTRA ADAUGARE PACIENT
+        adaugaPacientPanel = new JPanel(new MigLayout());
+
+        numeLabel = new JLabel("Nume");
+        prenumeLabel = new JLabel("Prenume");
+        cnpLabel = new JLabel("CNP");
+        nrTelefonLabel = new JLabel("Nr Telefon");
+        emailLabel = new JLabel("Email");
+
+        numeTF = new JTextField();
+        prenumeTF = new JTextField();
+        cnpTF = new JTextField();
+        nrTelefonTF = new JTextField();
+        emailTF = new JTextField();
+
+        adaugaPacient = new JButton("Adauga");
+
+        adaugaPacientPanel.add(numeLabel );
+        adaugaPacientPanel.add(numeTF,"span");
+        adaugaPacientPanel.add(prenumeLabel);
+        adaugaPacientPanel.add(prenumeTF, "span");
+        adaugaPacientPanel.add(cnpLabel);
+        adaugaPacientPanel.add(cnpTF, "span");
+        adaugaPacientPanel.add(nrTelefonLabel);
+        adaugaPacientPanel.add(nrTelefonTF, "span");
+        adaugaPacientPanel.add(emailLabel);
+        adaugaPacientPanel.add(emailTF, "span");
+        adaugaPacientPanel.add(adaugaPacient);
+
+        adaugaPacientFrame.setContentPane(adaugaPacientPanel);
+        adaugaPacientFrame.pack();
+        adaugaPacientFrame.setLocationRelativeTo(null);
+        adaugaPacientFrame.setResizable(false);
+
+        //// FINAL FEREASTRA ADAUGA PACIENT
     }
 
     public void updateProgramariTable() {
@@ -60,7 +166,6 @@ public class ReceptionerView extends BasicView{
         programariSP = new JScrollPane(programariTable);
         programariSP.setAutoscrolls(true);
         programariSP.setPreferredSize(new Dimension(920, 370));
-
     }
 
 
