a305da559e7b70a8e60d6da827292b2bb732c436
DESKTOP-L07CVPT\Tibi <tiberiubj@gmail.com>
2024-01-10 19:51:03 +0200

    trebuie adaugat in raportFrame chestiile alea

--- src/Controller/MedicController.java
+++ src/Controller/MedicController.java
@@ -2,8 +2,10 @@ package Controller;
 
 import Model.BigModel;
 import Model.MedicModel;
+import Model.ReceptionerModel;
 import View.BasicView;
 import View.MedicView;
+import View.ReceptionerView;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,20 +16,31 @@ import java.util.Date;
 
 public class MedicController extends BasicController implements ActionListener {
 
-    BasicView mv;
-    BigModel mm;
+//    BasicView mv;
+//    BigModel mm;
+    MedicView mv;
+    MedicModel mm;
 
     public MedicController(MedicView view, MedicModel model) {
         super(view,model);
 
-        this.mv = view;
-        this.mm = model;
+        mv = view;
+        mm = model;
+        mv.getRaportButton().addActionListener(this);
+        mv.getIstoricButton().addActionListener(this);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         super.actionPerformed(e);
+
+        if (e.getSource() == mv.getRaportButton()) {
+            mv.getRaportFrame().setVisible(true);
+        }
+        if (e.getSource() == mv.getIstoricButton()){
+            mv.getIstoricPacientFrame().setVisible(true);
+        }
     }
 
 }
--- src/View/MedicView.java
+++ src/View/MedicView.java
@@ -2,9 +2,11 @@ package View;
 
 import Model.CurrentAngajat;
 import net.miginfocom.swing.MigLayout;
+import org.jdesktop.swingx.JXDatePicker;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 import javax.imageio.ImageIO;
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
@@ -18,6 +20,7 @@ import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
@@ -27,6 +30,34 @@ import java.util.Objects;
 
 public class MedicView extends BasicView {
         private JPanel medicM3Panel;
+    private DefaultTableModel programariMedicTableModel;
+    private JScrollPane programariMedicSP;
+    private JTable programariMedicTable;
+    Object[][] programariMedicRowData = null;
+    private JButton raportButton;
+    private JButton istoricButton;
+
+        //// PENTRU RAPORT FRAME
+        private JFrame raportFrame;
+        private JLabel recomandatDeMedicLabel;
+        private JTextField recomandatDeMedicTF;
+        private JLabel medicCeAEfectuatLabel;
+        private JComboBox<String> medicCeAEfectuatCB;
+        private JLabel asistentMedicalLabel;
+        private JTextField asistentMedicalTF;
+        private JLabel dataEfectuariiLabel;
+        private JXDatePicker dataEfectuariiDP;
+        private JLabel istoricRelevantLabel;
+        private JTextField istoricRelevantTF;
+        private JButton investigatiiButton;
+        private JLabel diagnosticLabel;
+        private JTextField diagonsticTF;
+        private JLabel recomandariLabel;
+        private JTextField recomandariTF;
+        private JButton submitButton;
+
+        //// AM INCHIS PENTRU RAPORT FRAME
+        private JFrame istoricPacientFrame;
         @Override
         public void reAddToBV_RP_m3Panel()
         {
@@ -38,8 +69,69 @@ public class MedicView extends BasicView {
         }
         public  MedicView(CurrentAngajat currentAngajat){
             super("Proiect_V1_Policlinici_Medic",currentAngajat);
-            medicM3Panel = new JPanel(new MigLayout("insets 0"));
-            medicM3Panel.add(new JLabel("Hei ! sunt medic"));
+            medicM3Panel = new JPanel(new FlowLayout());
+
+            updateProgramariMedicTable();
+
+            raportFrame = new JFrame("Raport Medical");
+            raportFrame.setLayout(new MigLayout());
+
+            istoricPacientFrame = new JFrame("Istoric");
+
+            raportButton = new JButton("Raport");
+            istoricButton = new JButton("Istoric");
+
+            medicM3Panel.add(raportButton);
+            medicM3Panel.add(istoricButton, "span");
+            medicM3Panel.add(programariMedicSP);
+
+            recomandatDeMedicLabel = new JLabel("Recomandat de");
+            recomandatDeMedicTF = new JTextField("");
+            medicCeAEfectuatLabel = new JLabel("Medic ce a efectuat");
+
         }
 
+    public void updateProgramariMedicTable() {
+        String[] programariMedic = new String[]{"Nume", "Prenume", "Data", "Ora", "Serviciu"};
+
+        programariMedicTableModel = new DefaultTableModel(programariMedicRowData, programariMedic);
+        programariMedicTable = new JTable(programariMedicTableModel);
+
+        programariMedicTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        programariMedicTable.setDefaultEditor(Object.class, null);
+        programariMedicTable.getTableHeader().setReorderingAllowed(false);
+        programariMedicTable.setColumnSelectionAllowed(false);
+
+        programariMedicSP = new JScrollPane(programariMedicTable);
+        programariMedicSP.setAutoscrolls(true);
+        programariMedicSP.setPreferredSize(new Dimension(720, 520));
+    }
+
+    public JFrame getIstoricPacientFrame() {
+        return istoricPacientFrame;
+    }
+
+    public JFrame getRaportFrame() {
+        return raportFrame;
+    }
+
+    public JScrollPane getProgramariMedicSP() {
+        return programariMedicSP;
+    }
+
+    public Object[][] getProgramariMedicRowData() {
+        return programariMedicRowData;
+    }
+
+    public JTable getProgramariMedicTable() {
+        return programariMedicTable;
+    }
+
+    public JButton getRaportButton() {
+        return raportButton;
+    }
+
+    public JButton getIstoricButton() {
+        return istoricButton;
+    }
 }
