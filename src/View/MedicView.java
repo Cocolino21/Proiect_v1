de773b204a43125feb2aebc7a4681c7e88714d8a
DESKTOP-L07CVPT\Tibi <tiberiubj@gmail.com>
2024-01-11 18:21:22 +0200

    trebuie terminat investigatiiFrame.1

--- src/View/MedicView.java
+++ src/View/MedicView.java
@@ -221,6 +221,7 @@ public class MedicView extends BasicView {
         istoricTable.setColumnSelectionAllowed(false);
 
         istoricSP = new JScrollPane(istoricTable);
+
         istoricSP.setAutoscrolls(true);
         istoricSP.setPreferredSize(new Dimension(700, 400));
     }
